d3abfa2d01b1153d91b441ee62af11378f87eb42
XIAO100he <xiao100he@gmil.com>
2019-08-25 23:55:50 +0900

    Edit index.php

--- index.php
+++ index.php
@@ -9,26 +9,28 @@ require('head.php');
 	<?php
 	require('menu.php');
 	?>
-	<div class="top-image_wrapper">
-		<div class="swiper-btn_prev prev">
-			<i class="fas fa-caret-left"></i>
-		</div>
-		<div class="top-images">
-			<div class="image active">
-				<a href="#"><img src="https://d1f5hsy4d47upe.cloudfront.net/41/415b51b04a85b7ad1896ef0dc68bcf70_t.jpeg" alt=""></a>
+	<div class="index-wrapper">
+		<div class="top-image_wrapper">
+			<div class="swiper-btn_prev prev">
+				<i class="fas fa-caret-left"></i>
 			</div>
-			<div class="image">
-				<a href="#"><img src="https://d1f5hsy4d47upe.cloudfront.net/9a/9af7dc75443d0e55cb4c0a3f0913cb2d_t.jpeg" alt=""></a>
+			<div class="top-images">
+				<div class="image active">
+					<a href="#"><img src="https://d1f5hsy4d47upe.cloudfront.net/41/415b51b04a85b7ad1896ef0dc68bcf70_t.jpeg" alt=""></a>
+				</div>
+				<div class="image">
+					<a href="#"><img src="https://d1f5hsy4d47upe.cloudfront.net/9a/9af7dc75443d0e55cb4c0a3f0913cb2d_t.jpeg" alt=""></a>
+				</div>
+				<div class="image">
+					<a href="#"><img src="https://res-3.cloudinary.com/pocket-marche/image/upload/v1532757451/pvekz4jh969e90jj3z67.jpg" alt=""></a>
+				</div>
+				<div class="image">
+					<a href="#"><img src="https://www.design-47.com/wp-content/uploads/2019/01/%E3%82%BF%E3%82%AA%E3%83%AB.jpg" alt=""></a>
+				</div>
 			</div>
-			<div class="image">
-				<a href="#"><img src="https://res-3.cloudinary.com/pocket-marche/image/upload/v1532757451/pvekz4jh969e90jj3z67.jpg" alt=""></a>
+			<div class="swiper-btn_next next">
+				<i class="fas fa-caret-right"></i>
 			</div>
-			<div class="image">
-				<a href="#"><img src="https://www.design-47.com/wp-content/uploads/2019/01/%E3%82%BF%E3%82%AA%E3%83%AB.jpg" alt=""></a>
-			</div>
-		</div>
-		<div class="swiper-btn_next next">
-			<i class="fas fa-caret-right"></i>
 		</div>
 	</div>
 	<?php
