0ca47a787f4ffe9bd83e48d3fa31efafb29b03b3
XIAO100he <xiao100he@gmil.com>
2019-08-25 23:07:56 +0900

    menu.php

---
+++ menu.php
@@ -0,0 +1,12 @@
+<div class='menu-wrapper'>
+	<div class='menu-main'>
+		<ul>
+			<li class='menu'><a href="#" class='menu'><i class="fas fa-crown"></i>人気ランキング</a></li>
+			<li class='menu'><a href="#" class='menu'><span class='mrg-20'><i class="fas fa-gift"></i>お礼品でさがす</span></a></li>
+			<li class='menu'><a href="#" class='menu'><span class='mrg-20'><i class="fas fa-map-marker-alt"></i>地域でさがす</span></a></li>
+			<li class='menu'><a href="#" class='menu'><span class='mrg-20'><i class="fas fa-yen-sign"></i>金額でさがす</span></a></li>
+			<li class='menu'><a href="#" class='menu'><span class='mrg-20'><i class="far fa-handshake"></i>使い道でさがす</span></a></li>
+			<li class='menu'><a href="#" class='menu'><span class='mrg-20'><i class="fas fa-feather"></i>特集</span></a></li>
+		</ul>
+	</div>
+</div>
