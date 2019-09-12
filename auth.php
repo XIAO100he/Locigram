a70bb794766162297d16864eda4714fee62b2b07
XIAO100he <xiao100he@gmil.com>
2019-09-13 01:00:22 +0900

    Edit auth.php

--- auth.php
+++ auth.php
@@ -1,25 +1,27 @@
 <?php
+if( !empty($_SESSION['login_date']) ){
+	debug('ログイン済みユーザーです。');
 
-if(!empty($_SESSION['login_date'])){
-	debug('ログイン済みユーザーです');
-	
-	if(($_SESSION['login_data']+$_SESSION['login_limit']) < time()) {
-		debug('ログイン有効期限オーバーです');
+	if( ($_SESSION['login_date'] + $_SESSION['login_limit']) < time()){
+		debug('ログイン有効期限オーバーです。');
+
 		session_destroy();
-		header('Location:login.php');
-	} else {
-		debug('有効期限内です');
-		$_SESSION['login_data'] = time();
-	
-		if(basename($_SERVER['PHP_SELF'] === 'login.php')){
-			debug('インデックスページへ遷移します');
-			header('Location:index.php');
+
+		header("Location:login.php");
+	}else{
+		debug('ログイン有効期限以内です。');
+
+		$_SESSION['login_date'] = time();
+
+		if(basename($_SERVER['PHP_SELF']) === 'login.php'){
+			debug('マイページへ遷移します。');
+			header("Location:mypage.php"); 
 		}
 	}
-} else {
-	debug('未ログインユーザーです');
+}else{
+	debug('未ログインユーザーです。');
 	if(basename($_SERVER['PHP_SELF']) !== 'login.php'){
-		header('Location:login.php');
+		header("Location:login.php"); 
 	}
 }
 ?>
