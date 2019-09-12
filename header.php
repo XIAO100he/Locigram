b5f2ca8ad1374e1a1c6a250c9177fffbfae27df2
XIAO100he <xiao100he@gmil.com>
2019-09-13 01:06:46 +0900

    Edit header btn

--- header.php
+++ header.php
@@ -5,24 +5,21 @@
 	</div>
 	<div class="header-right">
 		<?php
-			if(empty($_SESSION['user_id'])){
+		if(empty($_SESSION['user_id'])){
 		?>
-		<p class="square">
-			<a href="login.php" class="login-btn">ログイン</a>
-		</p>
-		<p class="square">
-			<a href="signup.php" class="header-signup-btn">会員登録</a></p>
-		<p><i class="far fa-question-circle fa-fw"></i>よくある質問</p>
-		<p><i class="far fa-envelope fa-fw"></i>お問い合わせ</p>
-		<p><i class="far fa-building fa-fw"></i>会社情報</p>
+			<p class="square"><a href="login.php" class="login-btn">ログイン</a></p>
+			<p class="square"><a href="signup.php" class="header-signup-btn">会員登録</a></p>
+			<p><i class="far fa-question-circle fa-fw"></i>よくある質問</p>
+			<p><i class="far fa-envelope fa-fw"></i>お問い合わせ</p>
+			<p><i class="far fa-building fa-fw"></i>会社情報</p>
 		<?php
-			} else{
+			} else {
 		?>
-		<p class="square">ログアウト</p>
-		<p class="square"><a href="withdraw.php" class="login-btn">退会する</a></p>
-		<p><i class="far fa-question-circle fa-fw"></i>よくある質問</p>
-		<p><i class="far fa-envelope fa-fw"></i>お問い合わせ</p>
-		<p><i class="far fa-building fa-fw"></i>会社情報</p>
+		<p class="square"><a href="logout.php" class="logout-btn">ログアウト</a></p>
+			<p class="square"><a href="mypage.php" class="myp-btn">マイページ</a></p>
+			<p><i class="far fa-question-circle fa-fw"></i>よくある質問</p>
+			<p><i class="far fa-envelope fa-fw"></i>お問い合わせ</p>
+			<p><i class="far fa-building fa-fw"></i>会社情報</p>
 		<?php
 			}
 		?>
