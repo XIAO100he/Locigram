389f0f5f2033f9d4af73a2dffe19f8a82ca7b343
XIAO100he <xiao100he@gmil.com>
2019-09-13 11:13:28 +0900

    Make side-bar

---
+++ side_bar.php
@@ -0,0 +1,12 @@
+<div class="my-side_wrapper">
+	<div class="side-box">
+		<ul>
+			<li>▷カゴの中身</li>
+			<li>▷購入履歴</li>
+			<li>▷お気に入り</li>
+			<li>▷プロフィール</li>
+			<li>▷パスワード変更</li>
+			<li>▷退会手続</li>
+		</ul>
+	</div>
+</div>
