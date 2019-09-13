af11f9b7bdba7aae650f7991812840ff884553d6
XIAO100he <xiao100he@gmil.com>
2019-09-13 11:13:39 +0900

    Edit menu's css

--- menu.php
+++ menu.php
@@ -2,11 +2,11 @@
 	<div class='menu-main'>
 		<ul>
 			<li class='menu'><a href="#" class='menu'><i class="fas fa-crown"></i>人気ランキング</a></li>
-			<li class='menu'><a href="#" class='menu'><span class='mrg-20'><i class="fas fa-gift"></i>お礼品でさがす</span></a></li>
-			<li class='menu'><a href="#" class='menu'><span class='mrg-20'><i class="fas fa-map-marker-alt"></i>地域でさがす</span></a></li>
-			<li class='menu'><a href="#" class='menu'><span class='mrg-20'><i class="fas fa-yen-sign"></i>金額でさがす</span></a></li>
-			<li class='menu'><a href="#" class='menu'><span class='mrg-20'><i class="far fa-handshake"></i>使い道でさがす</span></a></li>
-			<li class='menu'><a href="#" class='menu'><span class='mrg-20'><i class="fas fa-feather"></i>特集</span></a></li>
+			<li class='menu'><a href="#" class='left-menu'><span class='mrg-20'><i class="fas fa-gift"></i>お礼品でさがす</span></a></li>
+			<li class='menu'><a href="#" class='left-menu'><span class='mrg-20'><i class="fas fa-map-marker-alt"></i>地域でさがす</span></a></li>
+			<li class='menu'><a href="#" class='left-menu'><span class='mrg-20'><i class="fas fa-yen-sign"></i>金額でさがす</span></a></li>
+			<li class='menu'><a href="#" class='left-menu'><span class='mrg-20'><i class="far fa-handshake"></i>使い道でさがす</span></a></li>
+			<li class='menu'><a href="#" class='left-menu'><span class='mrg-20'><i class="fas fa-feather"></i>特集</span></a></li>
 		</ul>
 	</div>
 </div>
