7aec4ca98491e371bc38754c2af6e1e4cb18e986
XIAO100he <xiao100he@gmil.com>
2019-09-13 17:18:41 +0900

    Edit side-bar

--- side_bar.php
+++ side_bar.php
@@ -1,12 +1,14 @@
 <div class="my-side_wrapper">
 	<div class="side-box">
-		<ul>
-			<li>▷カゴの中身</li>
-			<li>▷購入履歴</li>
-			<li>▷お気に入り</li>
-			<li>▷プロフィール</li>
-			<li>▷パスワード変更</li>
-			<li>▷退会手続</li>
+		<ul class='side-ul'>
+			<li class="side-menu">▷カゴの中身</li>
+			<li class="side-menu">▷購入履歴</li>
+			<li class="side-menu">▷お気に入り</li>
+			<li class="side-menu"><a href="profEdit.php" class="side-btn">▷プロフィール編集</a></li>
+			<li class="side-menu">▷パスワード変更</li>
+			<li class="side-menu"><a href="withdraw.php" class="side-btn">▷退会手続</a></li>
 		</ul>
 	</div>
 </div>
+
+
