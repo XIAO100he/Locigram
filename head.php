ab24bbde79bbdd5840b1b93329f722ef97d0db39
XIAO100he <xiao100he@gmil.com>
2019-09-16 23:23:42 +0900

    Fix head

--- head.php
+++ head.php
@@ -2,7 +2,7 @@
 <html lang="ja">
 	<head>
 		<meta charset="utf-8">
-		<title><?php echo $siteTitle; ?> | WEBUKATU MARKET</title>
+		<title><?php echo $siteTitle; ?> | Locigram</title>
 		<link rel="stylesheet" type="text/css" href="style.css">
 		<link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
 		<link href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" rel="stylesheet">
