aebefded719be385e8875947b88998a496fdde0a
XIAO100he <xiao100he@gmil.com>
2019-09-22 14:50:47 +0900

    Edit postHis

--- postHis.php
+++ postHis.php
@@ -7,11 +7,16 @@ debug('「　過去の投稿　');
 debug('「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「');
 debugLogStart();
 
-//================================
-// 画面処理
-//================================
-//ログイン認証
 require('auth.php');
+
+$u_id = $_SESSION['user_id'];
+
+$postData = getMyPosts($u_id);
+
+debug('取得した投稿データ：'.print_r($postData,true));
+
+debug('画面表示処理終了 <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<');
+
 ?>
 
 <!------------------------ビュー---------------------------------->
@@ -28,7 +33,26 @@ require('head.php');
 		<h2>過去の投稿</h2>
 		<!--メインボックス-->
 		<div class="my-main_wrapper">
-
+			<div class="myPost-wrapper">
+				<?php
+				if(!empty($postData)):
+				foreach($postData as $key => $val ):
+				?>
+				<div class="a_Post">
+					<a href="registPost.php<?php echo (!empty(appendGetParam())) ? appendGetParam().'&p_id='.$val['id'] : '?p_id='.$val['id']; ?>" class="panel">
+						<div class="panel-head">
+							<img src="<?php echo showImg(sanitize($val['pic1'])); ?>" alt="<?php echo sanitize($val['name']); ?>">
+						</div>
+						<div class="panel-body">
+							<p class="panel-title"><?php echo sanitize($val['title']); ?></p>
+						</div>
+					</a>
+				</div>
+				<?php
+				endforeach;
+				endif;
+				?>
+			</div>
 		</div>
 		<!--	サイドボックス-->
 		<?php
