5fc0891c996efb69cab5cc04c22c847305f5c356
XIAO100he <xiao100he@gmil.com>
2019-09-23 11:28:43 +0900

    Edit photos.php

--- photos.php
+++ photos.php
@@ -7,8 +7,14 @@ debug('「「「「「「「「「「「「「「「「「「「「「「「「�
 debugLogStart();
 
 $dbAllPosts = getPosts();
+debug('ポストの中身：'.print_r($dbAllPosts,true));
+
+$userId = array_column($dbAllPosts,'user_id');
+debug('ユーザーID：'.print_r($userId,true));
+
 
 ?>
+
 
 
 
@@ -17,12 +23,14 @@ if(!empty($dbAllPosts)):
 foreach($dbAllPosts as $key => $val ):
 ?>
 
+
 <div class="postCon-wrapper">
 	<div class="photo-wrapper">
-		<img src="<?php echo showImg(sanitize($val['pic1'])); ?>" alt="<?php echo sanitize($val['name']); ?>" alt="">
 		<a href="registPost.php<?php echo (!empty(appendGetParam())) ? appendGetParam().'&p_id='.$val['id'] : '?p_id='.$val['id']; ?>" class="panel">
+			<img src="<?php echo showImg(sanitize($val['pic1'])); ?>" alt="<?php echo sanitize($val['name']); ?>" alt="">
 			<div class="panel-body">
 				<p class="placeName"><span>@</span> <?php echo sanitize($val['title']); ?></p>
+				<p class="postUserName"></p>
 			</div>
 		</a>
 	</div>
