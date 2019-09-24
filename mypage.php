cb5802ee7af5d8851b92cf782f54ad793cbd9d20
XIAO100he <xiao100he@gmil.com>
2019-09-24 20:35:28 +0900

    Fix column name

--- mypage.php
+++ mypage.php
@@ -49,7 +49,7 @@ require('menu.php');
 				<div class="a_Post">
 					<a href="registPost.php<?php echo (!empty(appendGetParam())) ? appendGetParam().'&p_id='.$val['id'] : '?p_id='.$val['id']; ?>" class="panel">
 						<div class="panel-head">
-							<img src="<?php echo showImg(sanitize($val['pic1'])); ?>" alt="<?php echo sanitize($val['name']); ?>">
+							<img src="<?php echo showImg(sanitize($val['pic1'])); ?>" alt="<?php echo sanitize($val['title']); ?>">
 						</div>
 						<div class="panel-body">
 							<p class="panel-title"><?php echo sanitize($val['title']); ?></p>
