883aa1a8955821ca5dc114866ee5bb9c741cae1c
XIAO100he <xiao100he@gmil.com>
2019-09-24 20:35:38 +0900

    Edit photos.php

--- photos.php
+++ photos.php
@@ -9,25 +9,20 @@ debugLogStart();
 $dbAllPosts = getPosts();
 debug('ポストの中身：'.print_r($dbAllPosts,true));
 
-$userId = array_column($dbAllPosts,'user_id');
-debug('ユーザーID：'.print_r($userId,true));
-
-
+debug('画面表示処理終了 <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<');
 ?>
 
-
+<!------------------------ビュー---------------------------------->
 
 
 <?php
 if(!empty($dbAllPosts)):
 foreach($dbAllPosts as $key => $val ):
 ?>
-
-
 <div class="postCon-wrapper">
 	<div class="photo-wrapper">
-		<a href="registPost.php<?php echo (!empty(appendGetParam())) ? appendGetParam().'&p_id='.$val['id'] : '?p_id='.$val['id']; ?>" class="panel">
-			<img src="<?php echo showImg(sanitize($val['pic1'])); ?>" alt="<?php echo sanitize($val['name']); ?>" alt="">
+		<a href="postDetail.php<?php echo (!empty(appendGetParam())) ? appendGetParam().'&p_id='.$val['id'] : '?p_id='.$val['id']; ?>" class="panel">
+			<img src="<?php echo showImg(sanitize($val['pic1'])); ?>" alt="<?php echo sanitize($val['title']); ?>" alt="">
 			<div class="panel-body">
 				<p class="placeName"><span>@</span> <?php echo sanitize($val['title']); ?></p>
 				<p class="postUserName"></p>
