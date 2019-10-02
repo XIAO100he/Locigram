f5f244999959d58932e5c25e145494890ea28e87
XIAO100he <xiao100he@gmil.com>
2019-10-02 19:41:19 +0900

    開発環境のDB内容に書き換え

--- function.php
+++ function.php
@@ -217,23 +217,40 @@ function isLogin(){
 //================================
 //DB接続関数
 function dbConnect(){
-	$db = parse_url($_SERVER['CLEARDB_DATABASE_URL']);
-	$db['dbname'] = ltrim($db['path'], '/');
-	$dsn = "mysql:host={$db['host']};dbname={$db['dbname']};charset=utf8";
-	$user = $db['user'];
-	$password = $db['pass'];
+	$dsn = 'mysql:dbname=freemarket;host=localhost;charset=utf8';
+	$user = 'root';
+	$password = 'root';
 	$options = array(
-		// SQL実行失敗時にはエラーコードのみ設定（自分が指定したエラーコードの方に処理が移るようにする為）
+		// SQL実行失敗時にはエラーコードのみ設定
 		PDO::ATTR_ERRMODE => PDO::ERRMODE_SILENT,
 		// デフォルトフェッチモードを連想配列形式に設定
 		PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
 		// バッファードクエリを使う(一度に結果セットをすべて取得し、サーバー負荷を軽減)
 		// SELECTで得た結果に対してもrowCountメソッドを使えるようにする
-		PDO::MYSQL_ATTR_USE_BUFFERED_QUERY =>true,
+		PDO::MYSQL_ATTR_USE_BUFFERED_QUERY => true,
 	);
-	$dbh = new PDO($dsn,$user,$password,$options);
+	// PDOオブジェクト生成（DBへ接続）
+	$dbh = new PDO($dsn, $user, $password, $options);
 	return $dbh;
 }
+//Heroku本番環境用
+//	$db = parse_url($_SERVER['CLEARDB_DATABASE_URL']);
+//	$db['dbname'] = ltrim($db['path'], '/');
+//	$dsn = "mysql:host={$db['host']};dbname={$db['dbname']};charset=utf8";
+//	$user = $db['user'];
+//	$password = $db['pass'];
+//	$options = array(
+//		// SQL実行失敗時にはエラーコードのみ設定（自分が指定したエラーコードの方に処理が移るようにする為）
+//		PDO::ATTR_ERRMODE => PDO::ERRMODE_SILENT,
+//		// デフォルトフェッチモードを連想配列形式に設定
+//		PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+//		// バッファードクエリを使う(一度に結果セットをすべて取得し、サーバー負荷を軽減)
+//		// SELECTで得た結果に対してもrowCountメソッドを使えるようにする
+//		PDO::MYSQL_ATTR_USE_BUFFERED_QUERY =>true,
+//	);
+//	$dbh = new PDO($dsn,$user,$password,$options);
+//	return $dbh;
+//}
 
 
 //SQL実行関数
